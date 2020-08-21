6e1bba8e933abf56ca6df51eaf20ba28fe4265ef
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-21 10:37:29 -0700

    Add function to create array, and fill with user input, decrements through to zero, and zero.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,26 @@ $(document).ready(function () {
   $("form#usernumbers").submit(function (event) {
     event.preventDefault();
     const usernumber = parseInt($("input#numberinput").val());
-  });
+    let robogersresponse = ["Beep!", "Boop!", "Won't you be my neighbor?"]
+
+    function computeresponse(usernumber) {
+      let residualarray = []
+      let numberremainingtoprint = parseInt($("input#numberinput").val());
+      const printedresponse = robogersresponse.map(function (robogersresponse) {
+
+        for (numberremainingtoprint = usernumber); {
+          residualarray.unshift(usernumber)
+          numberremainingtoprint--;
+        };
+
+        for (numberremainingtoprint > 0 < usernumber) {
+          residualarray.push(numberremainingtoprint--);
+        }
+
+        for (numberremainingtoprint = 0); {
+          residualarray.push(0);
+        }
+      }
+    });
+});
 });
