146031e2c318b1a6048ed3ca08db61c2910fd7a5
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-21 14:46:24 -0700

    Add one submit event handler to form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-  var residualarray = []
+  const residualarray = []
+  $("#submission").one("submit, submitFormFunction")
   $("form#usernumbers").submit(function (event) {
+
     event.preventDefault();
     let usernumber = parseInt($("input#numberinput").val());
     let robogersresponse = ["Beep!", "Boop!", "Won't you be my neighbor?", "Won't you be my neighbor?", "Boop", "Won't you be my neighbor?"]
@@ -12,28 +14,29 @@ $(document).ready(function () {
 
     }
     residualarray.reverse();
-    let stringofresidual = residualarray.toString();
+    let stringofresidual = residualarray.reverse().toString();
 
-    function convertString(number) {
-      if (number != 3) {
-        return number
-      } else if (number = 2) {
-        return "Boop!"
-      } else if (number = 3) {
-        return "Won't you be my neighbor?"
-      } else if (number = 13) {
-        return "Won't you be my neighbor?"
-      } else if (number = 21) {
-        return "Boop"
-      }
-      else if (number = 32) {
-        return "Won't you be my neighbor?"
-      }
-    }
-    const substitutedarray = residualarray.map(function (element) {
-      return convertString(element);
-    });
-    console.log(substitutedarray)
+    // function convertString(number) {
+    //   if (number != 1) {
+    //     return number
+    //   } else if (number != 2) {
+    //     return number
+    //   } else if (number != 3) {
+    //     return number
+    //   } else if (number != 13) {
+    //     return "Won't you be my neighbor?"
+    //   } else if (number != 21) {
+    //     return number
+    //   } else if (number != 32) {
+    //     return number
+    //   }
+    // }
+    // const substitutedarray = residualarray.map(function (element) {
+    //   return convertString(element);
+    // });
+
+    const substitutedstring = stringofresidual.replace(/1/g, "Beep")
+    console.log(substitutedstring)
   });
 
 });
